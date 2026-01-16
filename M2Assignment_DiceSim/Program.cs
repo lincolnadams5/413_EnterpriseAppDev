b7b4c4f91590413a0ced6effeadb604f3b11b70d
Lincoln Adams <153785479+lincolnadams5@users.noreply.github.com>
2026-01-16 11:13:50 -0700

    Finished Mission 2 Dice Sim Assignment

---
+++ M2Assignment_DiceSim/Program.cs
@@ -0,0 +1,38 @@
+﻿using M2Assignment_DiceSim;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Welcome statements
+        Console.WriteLine($"Welcome to Lincoln's double dice simulator!\n");
+        Console.WriteLine($"How many dice rolls would you like to simulate?");
+
+        // Get user input for dice rolls
+        int numRolls;
+        numRolls = int.Parse(Console.ReadLine());
+
+        Sim sim = new Sim(); // Creating new instance of the "Sim" class
+        int[] diceRolls = sim.DiceRoll(numRolls); // Assigning the return array of dice rolls to an array
+
+        PrintHist(diceRolls, numRolls); // Calls method to print histogram based on percentage of total rolls and simulated frequency
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+
+    private static void PrintHist(int[] diceRolls, int numRolls)
+    {
+        int percentage = 0; // Initializes percentage variable
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine($"Total number of rolls = {numRolls}.");
+        Console.WriteLine();
+
+        for (int i = 2; i <= 12; i++) // Loops through 2-12 (possible dice results)
+        {
+            percentage = (int)((diceRolls[i] / (double)numRolls) * 100); // Converts results to percentage of total
+            Console.WriteLine(i + ": " + new string('*', percentage)); // Prints frequency as histogram
+        }
+    }
+}
---
+++ M2Assignment_DiceSim/Sim.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace M2Assignment_DiceSim
+{
+    public class Sim
+    {
+        public int[] DiceRoll(int rolls)
+        {
+            int[] results = new int[13]; // Initialize an empty array where each index will store the frequency of the roll
+            int num = 0; // Will store the temporary value of the roll
+
+            for (int i = 0; i < rolls; i++) // For the amount of rolls specified by the user
+            {
+                num = Random.Shared.Next(1, 7) + Random.Shared.Next(1, 7); // Simulates dice roll and stores in temporary variable
+                results[num] += 1; // Increments frequency by 1 at the index of the result (0 and 1 will remain 0: impossible rolls)
+            }
+            return results; // Returns results
+        }
+    }
+}
