6a0475824fee0a439bf6b10d270f178e0f8864eb
Lincoln Adams <153785479+lincolnadams5@users.noreply.github.com>
2026-01-16 15:54:25 -0700

    Video walkthrough for Mission 3

---
+++ Mission_3/VideoWalkthrough_M3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using VideoWalkthrough_M3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* User enters quote and program returns the following:
+            * Word count
+            * Character count
+            * Letter frequency
+        */
+
+        WordTools wt = new WordTools();
+
+        string quote = "";
+
+        Console.WriteLine("Please enter a quote: ");
+        quote = Console.ReadLine();
+
+        Console.WriteLine("Number of Words: " + wt.WordCounter(quote));
+        Console.WriteLine("Number of Characters: " + wt.CharCounter(quote));
+        Console.WriteLine("Character Frequency: \n\n" + wt.AlphCounter(quote));
+    }
+}
---
+++ Mission_3/VideoWalkthrough_M3/WordTools.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoWalkthrough_M3
+{
+    internal class WordTools
+    {
+        public int WordCounter(string words)
+        {
+            int wordCount = 0;
+
+            string[] individualWords = words.Split(' ');
+
+            wordCount = individualWords.Length;
+
+            return wordCount;
+        }
+
+        public int CharCounter (string words)
+        {
+            int charCount = 0;
+
+            charCount = words.Length;
+
+            return (charCount);
+        }
+
+        public string AlphCounter (string words)
+        {
+            string resultString = "";
+            string w = words.ToLower();
+            var hashMap = new Dictionary<char, int>();
+
+            // Loop through the string
+            for (int i = 0; i < words.Length; i++)
+            {
+                char c = words[i];
+
+                if (hashMap.ContainsKey(c))
+                {
+                    hashMap[c] += 1;
+                }
+                else
+                {
+                    hashMap.Add(c, 1);
+                }
+            }
+
+            foreach (var (key, value) in hashMap)
+            {
+                resultString += $"{key}: {value}\n";
+            }
+
+            return resultString;
+        }
+    }
+}
